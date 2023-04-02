977a36048a29cda9f83432a12935b193dbd79bd7
Grzegorz Ozimek <hexaferroon@gmail.com>
2023-04-02 17:02:46 +0200

    added saving to df, sqlite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from modules import reports_parser
 from modules.useful_methods import get_list_of_reports, get_unique_list
+import pandas as pd
+import sqlite3
 
 
 REPORT_PATH = "./input/"
@@ -7,21 +9,73 @@ REPORT_PATH = "./input/"
 
 if __name__ == "__main__":
     list_of_reports = get_list_of_reports(REPORT_PATH)
-    for report in list_of_reports:
-        print(f"{report=}\n")
+    # for report in list_of_reports:
+    #     print(f"{report=}\n")
         
-    file_name = list_of_reports[0]
-    pdf_report = reports_parser.CMMReport(file_name)
-    # pdf_report.show_blocks_text()
-    pdf_report.blocks_to_df()
+    # file_name = list_of_reports[0]
+    # pdf_report = reports_parser.CMMReport(file_name)
+    # pdf_report.show_blocks_text2()
+    # pdf_report.show_raw_text()
+    # pdf_report.blocks_to_df()
     # pdf_report.show_df()
     # pdf_report.export_to_excel()
+    # pdf_report.blocks_to_dict()
+    # pdf_report.show_dict()
+    # pdf_report.blocks_to_sqlite()
     
-    # dumping blocks to txt, just for testing
-    with open("dump.txt", "w", encoding="utf-8") as f:
-        for block in pdf_report.pdf_blocks_text:
-            for line in block:
-                items_str = ' '.join([str(item) for item in line])
-                f.write(items_str)
-                f.write("\n")
-            f.write("\n")
+    # df = pd.DataFrame(pdf_report.dict_list)
+    # print(df)
+    
+    # # dumping blocks to txt, just for testing
+    # with open("dump.txt", "w", encoding="utf-8") as f:
+    #     for block in pdf_report.pdf_blocks_text:
+    #         for line in block:
+    #             items_str = ' '.join([str(item) for item in line])
+    #             f.write(items_str)
+    #             f.write("\n")
+    #         f.write("\n")
+    
+    # def list_to_dict_dataframe(data):
+    #     header = [item for sublist in data[0] for item in sublist if isinstance(item, str) and item.startswith(('*', '#', 'DIM'))]
+    #     header = ', '.join(header)
+    #     columns = ['AX', 'NOM', '+TOL', '-TOL', 'BONUS', 'MEAS', 'DEV', 'OUTTOL']
+    #     df = pd.DataFrame(data[1], columns=columns)
+    #     return header, df
+    
+    # def list_to_dict_dataframe(data):
+    #     header = [item for sublist in data[0] for item in sublist if isinstance(item, str) and item.startswith(('*', '#', 'DIM'))]
+    #     header = ', '.join(header)
+    #     columns = ['AX', 'NOM', '+TOL', '-TOL', 'BONUS', 'MEAS', 'DEV', 'OUTTOL']
+    #     df = pd.DataFrame(data[1], columns=columns)
+    #     df.columns.name = header
+    #     return df
+     
+    df_list = []
+    for report in list_of_reports:
+        print(f"{report=}")
+        pdf_report = reports_parser.CMMReport(report)
+        # pdf_report.show_blocks_text2()
+        pdf_report.to_sqlite()
+        pdf_report.to_df()
+        df_list.append(pdf_report.df)
+    
+    # connect to the database
+    conn = sqlite3.connect('mydatabase.db')
+
+    # get a list of all table names in the database
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = [table[0] for table in cursor.fetchall()]
+
+    # iterate over the tables and extract data
+    for table in tables:
+        # extract data from the table and store it in a Pandas DataFrame
+        df = pd.read_sql_query(f'SELECT * FROM "{table}"', conn)
+        # print(f"Table name: {table}")
+        print(df)
+
+    # close the database connection
+    conn.close()
+        
+    result_df = pd.concat(df_list)
+    result_df.to_excel('output.xlsx', index=False)
--- modules/reports_parser.py
+++ modules/reports_parser.py
@@ -1,5 +1,6 @@
 import pdfplumber
 import pandas
+import sqlite3
 
 
 class CMMReport:
@@ -16,7 +17,7 @@ class CMMReport:
         self.pdf_raw_text = []
         self.pdf_blocks_text = []
         self.headers_list = []
-        self.df_measurements = pandas.DataFrame()
+        self.df = pandas.DataFrame()
 
         self.cmm_open()
         self.split_text_to_blocks()
@@ -45,58 +46,63 @@ class CMMReport:
             for line in block:
                 print(f"{line} ({len(line)=})")
             print(f"___[END OF BLOCK ({len(block)=})]___\n")
+            
+    def show_blocks_text2(self):
+        """Method to print pdf_blocks_text - blocks of measurements
+        """
+        for block in self.pdf_blocks_text:
+            print("\n___[BEGINNING OF BLOCK]___")
+            print(f"{block}")
+            print(f"___[END OF BLOCK ({len(block)=})]___\n")
 
     def split_text_to_blocks(self):
         """Method to split raw text from pdf to blocks - split by measurements
             Each block is created as follows:
-            block[0] - headers list
-            block[1] - headers names list
-            block[2:] - dimensions
+            block[0] - header
+            block[1] - dimensions
         """
         text_block = []
         block_headers = []
         dim_block = []
         prev_line = " "
         
-        for line in self.pdf_raw_text:
-            if line[0] != "#" and line[0] != "*":
-                temp_line_split = line.split()
-                if len(temp_line_split)==3:
+        for index, line in enumerate(self.pdf_raw_text):
+            if index == len(self.pdf_raw_text) - 1:
+                if text_block:
+                    text_block.append(dim_block)
+                    text_block.insert(1, block_headers)
+                    text_block.pop(0)
+                    self.pdf_blocks_text.append(text_block)
+                    text_block, block_headers, dim_block = [], [], []
+                    block_headers.append([line])
+                    
+            if not line.startswith(('#', '*')):
+                if len(line.split()) == 3:
                     continue
 
             if len(text_block) > 0:
-                if line[0] == "#" or line[0] == "*":
-                    if prev_line[0] == "#" or prev_line[0] == "*":
+                if line[0] in {"#", "*"}:
+                    if prev_line and prev_line[0] in {"#", "*"}:
                         block_headers.append([line])
                         
                     else:
                         text_block.append(dim_block)
                         text_block.insert(1, block_headers)
                         text_block.pop(0)
-                                                
                         self.pdf_blocks_text.append(text_block)
-                                                
-                        text_block = []
-                        block_headers = []
-                        dim_block = []
-                        
+                        text_block, block_headers, dim_block = [], [], []
                         block_headers.append([line])
-    
-                elif line[0:3] == "DIM":
-                    if len(dim_block) > 0:
+
+                elif line.startswith("DIM"):
+                    if dim_block:
                         text_block.append(dim_block)
                         dim_block = []
-                        
                     block_headers.append([line])
-                    block_headers.append(["AX",  "NOM", "+TOL", "-TOL", "BONUS", "MEAS", "DEV", "OUTTOL"])
                     
                 else:
                     temp_line = []
                     line = line.split()
-                    
-                    for item in line:
-                        if item[0:2] == "--" or item[0:2] == "-#" or item[0:2] == "#-":
-                            line.remove(item)
+                    line = [item for item in line if not (item.startswith("--") or item.startswith("-#") or item.startswith("#-") or item.startswith("<-"))]
 
                     """
                     temp_line[0] = dimension symbol
@@ -116,7 +122,7 @@ class CMMReport:
                         temp_line = [line[0], float(line[1]), float(line[2]), float(line[3]), "", float(line[4]), float(line[5]), float(line[6])]
 
                     elif line[0] == "TP" and len(line) == 7:
-                        temp_line = [line[0], line[1], float(line[2]), "", float(line[3]), float(line[4]), float(line[5]), float(line[6])]
+                        temp_line = [line[0], "", float(line[2]), "", float(line[3]), float(line[4]), float(line[5]), float(line[6])]
 
                     elif line[0] == "M" and len(line) == 7:
                         temp_line = [line[0], float(line[1]), float(line[2]), float(line[3]), "", float(line[4]), float(line[5]), float(line[6])]
@@ -132,6 +138,9 @@ class CMMReport:
 
                     elif line[0] == "DF" and len(line) == 8:
                         temp_line = [line[0], float(line[1]), float(line[2]), float(line[3]), float(line[4]), float(line[5]), float(line[6]), float(line[7])]
+                        
+                    elif line[0] == "DF" and len(line) == 7:
+                        temp_line = [line[0], float(line[1]), float(line[2]), "", float(line[3]), float(line[4]), float(line[5]), float(line[6])]
 
                     elif line[0] == "PR" and len(line) == 7:
                         temp_line = [line[0], float(line[1]), float(line[2]), float(line[3]), "", float(line[4]), float(line[5]), float(line[6])]
@@ -147,16 +156,11 @@ class CMMReport:
                     
                     if temp_line:
                         dim_block.append(temp_line)
-                    
+                               
             else:
-                if line[0] == "#" or line[0] == "*":
-                    block_headers.append([line])
-                        
-                elif line[0:3] == "DIM":
-                    block_headers.append([line])
-                    block_headers.append(["AX",  "NOM", "+TOL", "-TOL", "BONUS", "MEAS", "DEV", "OUTTOL"])
-                
-                text_block.append([])
+                if line[0] in {"#", "*"} or line.startswith("DIM"):
+                    block_headers.append(line)
+                text_block.append(block_headers)
 
             prev_line = line
                 
@@ -168,34 +172,81 @@ class CMMReport:
         """
         
         return self.headers_list
+    
+    def to_dict(self):
+        pass
 
-    def blocks_to_df(self):
-        """This till be method to create Pandas' DataFrame from text blocks
+    def to_df(self):
+        """This method converts blocks to dataframe
         """
-        for block in self.pdf_blocks_text:            
-            block_headers = []
-            for element in block[0]:
-                tmp_header = []
-                
-                if element[0] != "AX":
-                    tmp_header = (element[0], "", "", "", "", "", "", "")
+        df_list = []
+        for block in self.pdf_blocks_text:
+            header = ""
+            for sublist in block[0]:
+                if isinstance(sublist, str):
+                        header += sublist
+                        header += ", "
                 else:
-                    tmp_header = (element[0], element[1], element[2], element[3], element[4], element[5], element[6], element[7])
-                    
-                block_headers.append(tmp_header)
-                                    
-            block_df = pandas.DataFrame(block[1:][0])
-            block_df.columns = pandas.MultiIndex.from_arrays(block_headers)
-                        
-            self.df_measurements = pandas.concat([self.df_measurements, block_df], axis=1)
+                    for item in sublist:
+                        if isinstance(item, str):
+                            header += item
+                            header += ", " 
+            #header = [item for sublist in block[0] for item in sublist if isinstance(item, str)]
+            #header = ', '.join(header)
+            header = header[:-2]
+            columns = ['AX', 'NOM', '+TOL', '-TOL', 'BONUS', 'MEAS', 'DEV', 'OUTTOL']
+            df = pandas.DataFrame(block[1], columns=columns)
+            df['Header'] = header
+            df['File'] = self.pdf_file_path
+            df_list.append(df)
+
+        if df_list:
+            self.df = pandas.concat(df_list)
+            
+    def to_sqlite(self):
+        # Define the database connection and cursor
+        conn = sqlite3.connect('mydatabase.db')
+        cursor = conn.cursor()
+
+        # Loop over the lists and insert them into the database
+        for lst in self.pdf_blocks_text:
+            table_name = ""
+            # Extract the table name from the first sublist
+            for sublist in lst[0]:
+                if isinstance(sublist, str):
+                        table_name += sublist
+                        table_name += ", "
+                else:
+                    for item in sublist:
+                        if isinstance(item, str):
+                            table_name += item
+                            table_name += ", "           
+            #table_name = [item for sublist in lst[0] for item in sublist if isinstance(item, str)]
+            #table_name = ', '.join(table_name)
+            table_name = table_name.replace('"', '')
+            table_name = table_name[:-2]
+            # print(f"{table_name=}")
+            # print(f"{lst=}")
+            #table_name = "test"
+            
+            # Create the table if it does not exist
+            cursor.execute(f'CREATE TABLE IF NOT EXISTS "{table_name}" (AX TEXT, NOM REAL, PLUSTOL REAL, MINUSTOL REAL, BONUS REAL, MEAS REAL, DEV REAL, OUTTOL REAL)')
+            
+            # Insert the data into the table
+            for row in lst[1]:
+                cursor.execute(f'INSERT INTO "{table_name}" VALUES (?, ?, ?, ?, ?, ?, ?, ?)', row)
+
+        # Commit the changes and close the connection
+        conn.commit()
+        conn.close()
                            
     def show_df(self):
         """Method to print dataframe with measurements
         """
-        print(f"{self.df_measurements}")
+        print(f"{self.df}")
                   
     def export_to_excel(self):
         """Method to export generated DataFrame to Excel file
         """
         ###TODO: something more elegant
-        self.df_measurements.to_excel("dump.xlsx")
+        self.df.to_excel("dump.xlsx", index=False)
