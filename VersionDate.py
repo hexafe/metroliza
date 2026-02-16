edb161899781de6484ef98e4d098aeb508b058ad
hexafe <100516322+hexafe@users.noreply.github.com>
2026-02-16 19:14:38 +0100

    Update VERSION_DATE and release notes for new version

--- VersionDate.py
+++ VersionDate.py
@@ -1,7 +1,10 @@
-VERSION_DATE = "240225"
+VERSION_DATE = "260216"
 
 release_notes = f"""
-    <br><b>Current Version 240225:</b><br>
+    <br><b>Current Version 260216:</b><br>
+    - Bugfixes and improvements (grouping, interfaces between modules)<br>
+    
+    <br><b>Version 240225:</b><br>
     - Added CSV module prototype for testing<br>
     
     <br><b>Version 240218:</b><br>
