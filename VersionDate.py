55f392b16f90b2129093874aecfa384fba849bfa
hexafe <100516322+hexafe@users.noreply.github.com>
2026-02-23 19:40:56 +0100

    Update VERSION_DATE to 260223

--- VersionDate.py
+++ VersionDate.py
@@ -1,7 +1,7 @@
-VERSION_DATE = "260222"
+VERSION_DATE = "260223"
 
 release_notes = """
-    <br><b>Current Version 260222:</b><br>
+    <br><b>Current Version 260223:</b><br>
     - Performance optimizations<br>
 
     <br><b>Version 260216:</b><br>
