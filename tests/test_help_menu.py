a18f89683a461a6cef520c1bde610eb8ac9e47c2
hexafe <100516322+hexafe@users.noreply.github.com>
2026-03-22 23:44:04 +0100

    Fix manual links to use master branch

--- tests/test_help_menu.py
+++ tests/test_help_menu.py
@@ -81,7 +81,7 @@ class TestHelpMenu(unittest.TestCase):
         for key, path in self.help_menu.MANUAL_PATHS.items():
             with self.subTest(key=key):
                 expected = (
-                    'https://github.com/hexafe/metroliza/blob/main/'
+                    'https://github.com/hexafe/metroliza/blob/master/'
                     f'{path.relative_to(self.help_menu.REPO_ROOT).as_posix()}'
                 )
                 self.assertEqual(self.help_menu.manual_url(key), expected)
